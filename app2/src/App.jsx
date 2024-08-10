f446f9067c09ac61e9b951154b4a281e32dda38a
aya-soghayyer <128791822+aya-soghayyer@users.noreply.github.com>
2024-08-10 08:25:38 +0200

    part two useEffect live search ;)

--- app2/src/App.jsx
+++ app2/src/App.jsx
@@ -1,33 +1,54 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import axios from 'axios'
+
+
 
 function App() {
   
   const [count, setCount] = useState(0)
-  const [searchQuery, setSearchQuery] = useState("")
+  const [searchQuery, setSearchQuery] = useState('')
+  const [products, setProducts] = useState([])
+
+  const getProduct = async()=>{
+    const response = await fetch(`https://dummyjson.com/products/1`)
+    const data =await response.json()
+    console.log(data)
+  }
+
+  const getSearchProduct =  async()=>{
+    const {data} = await axios.get(`https://dummyjson.com/products/search?q=${searchQuery}`)
+        setProducts(data.products)
+        console.log(data.products)
+}
+  
+  useEffect(()=>{
+    getProduct()
+  },[])
 
   useEffect(()=>{
-    console.log('useEffect with searchquery')
-  }, [searchQuery])
-
-  useEffect(()=>{
-    console.log('useEffect with searchquery or count')
-  }, [count, searchQuery])
-
-    useEffect(()=>{
-      console.log('useEffect with count')
-    }, [count])
-
-  useEffect(()=>{
-    console.log('useEffect')
-  })
+    getSearchProduct()
+  },[searchQuery])
 
   return (
-    <>
+    <> 
     <button onClick={()=>{
       setCount(count +1)
     }} >click {count}</button>
-    <div>App</div>
+    
     <input type="text"  value={searchQuery} onChange={(e)=>{setSearchQuery(e.target.value)}}/>
+
+   <button onClick={getSearchProduct}>search</button>
+    { products.map((product)=>(
+  
+      <div className="product" key={product.id}>
+      <h1>{product.title}</h1>
+      <p>{product.description}</p>
+      <img src={product.thumbnail}  />
+      </div>
+    ))}
+        
+    
+    
     </>
   
   )
